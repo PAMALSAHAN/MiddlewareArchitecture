50fa95bc0b080e5d59c5f82716b41bb557c343cd
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-05-24 23:07:37 +0530

    adding dependancy container

---
+++ Dependency Injection Explained with Lifetimes Singleton-Transient (C# Example)/1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            // var service=new HellowService();
+            // service.PrintHellow();  
+
+            // var customer=new HellowCustomer(service);
+            // customer.CustomerPrint();
+
+            //using activators
+            // var service=(HellowService)Activator.CreateInstance(typeof(HellowService));
+            // var customer=(HellowCustomer)Activator.CreateInstance(typeof(HellowCustomer),service);
+            // service.PrintHellow();
+            // customer.CustomerPrint();
+
+            ///making container
+            var container=new DependancyContainer();
+            container.AddDependancy(typeof(HellowCustomer));
+            container.AddDependancy(typeof(HellowService));
+
+        }
+    }
+
+    //dependancy container class
+    internal class DependancyContainer
+    {
+        List<Type> _dependancy;
+        public void AddDependancy(Type type)
+        {
+            _dependancy=new List<Type>();
+            _dependancy.Add(type);
+        }
+        
+        public Type GetDependancy(Type type)
+        {
+            return _dependancy.Find(x=>x.Name==type.Name);
+        }
+    }
+
+    
+
+
+    internal class HellowCustomer
+    {
+        private HellowService hellowService;
+
+        public HellowCustomer(HellowService hellowService)
+        {
+            this.hellowService = hellowService;
+        }
+        
+        public void CustomerPrint( )
+        {
+            hellowService.PrintHellow();
+        }
+        
+    }
+
+    internal class HellowService
+    {
+        public HellowService()
+        {
+        }
+
+        public void PrintHellow( )
+        {
+            System.Console.WriteLine("hellow from hellowService");
+        }
+    }
+}
