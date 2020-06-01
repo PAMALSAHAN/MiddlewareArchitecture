b63ad045fa3d1c3082614810b1b8f4cb28863771
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-06-01 20:36:46 +0530

    async await task basic finished

--- C# Async-Await-Task Explained (Deep Dive)/2/Program.cs
+++ C# Async-Await-Task Explained (Deep Dive)/2/Program.cs
@@ -5,28 +5,86 @@ namespace _2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            MakeCupOfTea();
+            await MakeCupOfTeaAsync();
+            //we use Async as lastword of the function. 
+            //if you dontwant you can remove it.
         }
-        public static string MakeCupOfTea()
+        public static async Task<string> MakeCupOfTeaAsync()
         {
-            var boiledWater=WaterBoiling("water");
+            var boilingWater=WaterBoiling("water");
             System.Console.WriteLine("take two cups");
             System.Console.WriteLine("put tea packets in to two cups");
+
+            System.Console.WriteLine("starts making breakfast");
+            Task.Delay(5000).GetAwaiter().GetResult();
+            System.Console.WriteLine("finish making breakfast");
+
+            for (int i = 0; i < 100; i++)
+            {
+                System.Console.WriteLine(i);
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
+
+
+            var boiledWater=await boilingWater; //with out returning the return of waterboiling
+            //bellow stepes not work. 
+            System.Console.WriteLine("arranging table");
+            Task.Delay(1000).GetAwaiter().GetResult();
+            System.Console.WriteLine("sind invitotion to friends");
+
             System.Console.WriteLine("put "+boiledWater+" in to cups");
             return "tea";
         }
 
-        private static string WaterBoiling(string water)
+        private static async Task<string> WaterBoiling(string water)
         {
+            //we use Async as lastword of the function. 
+            //if you dontwant you can remove it.
+
             System.Console.WriteLine("start the kettle");
-            Task.Delay(4000).GetAwaiter().GetResult();
+            await Task.Delay(4000);
             //propagates exceptions rather than wrapping them in an AggregateException
             // use it for best practise
+        
+            for (int i = 0; i < 100; i++)
+            {
+                System.Console.WriteLine("boiling");
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
             System.Console.WriteLine("finished boiling");
+
+
+        
             return "boiled water";
 
         }
     }
 }
+
+// start the kettle
+// take two cups
+// put tea packets in to two cups
+// starts making breakfast
+// boiling
+// finish making breakfast
+// boiling
+// 0
+// 1
+// boiling
+// 2
+// .......
+// ... 
+// .. 
+
+// 96
+// boiling
+// 97
+// boiling
+// 98
+// finished boiling
+// 99
+// arranging table
+// sind invitotion to friends
+// put boiled water in to cups
---
+++ C# Async-Await-Task Explained (Deep Dive)/3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace _3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"1");
+            var client=new HttpClient();
+            System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"2");
+            var tasks=client.GetStringAsync("https://www.google.com");
+
+            System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"3");
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(i);
+                
+            }
+            System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"4");
+            var page=await tasks;
+            System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId+"5");
+            
+
+        }
+    }
+}
