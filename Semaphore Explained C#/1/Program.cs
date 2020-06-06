4747468a0a0dd9a841eec46619abec4740af0a97
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-06-06 18:44:01 +0530

    init semaphore

---
+++ Semaphore Explained C#/1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SemaphoreSlim gate=new SemaphoreSlim(2);
+            // System.Console.WriteLine("start");
+            // await gate.WaitAsync();
+            // System.Console.WriteLine("do some work");
+            // System.Console.WriteLine("finish");
+
+
+            // this using for loop
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("start");
+                await gate.WaitAsync();
+                System.Console.WriteLine("do some work");
+                System.Console.WriteLine("finish");
+            }
+
+        }
+    }
+}
